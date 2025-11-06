afee47a8068ba6e8973e1364c4dc386f19d3a2ff
Nguyende289 <ngde.bg@gmail.com>
2025-11-06 16:56:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const parseCSV = (text) => {
 };
 
 // ID của Google Sheet được chỉ định sẵn
-const GOOGLE_SHEET_ID = '1IBNYEMAIHjzUPSEcG1NIvn80t_vJO0_pFYiTcWquRH4';
+const GOOGLE_SHEET_ID = '1pjutzB4Bc9WBtqmA3MUoxQ7CoZpBvHBMGR1OxaZQENE';
 
 const Clock = () => {
     const [time, setTime] = React.useState(new Date());
